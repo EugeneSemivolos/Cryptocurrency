206a2390650dc54a34fee036152b94dac0333a6b
EugeneSemivolos <semivolos.evgen1@gmail.com>
2024-05-24 16:15:02 +0300

    implemented layout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import 'antd/dist/reset.css';
 
 ReactDom.createRoot(document.getElementById("root")).render(
-  <App />
+  <Router>
+    <App />
+  </Router>
 );
