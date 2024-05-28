391efcd03905a08d9f3dcc404d9d1832cc0fd721
EugeneSemivolos <semivolos.evgen1@gmail.com>
2024-05-28 11:03:41 +0300

    finished homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 
 const { Title } = Typography;
 
@@ -23,8 +24,19 @@ const Homepage = () => {
         <Col span={12}><Statistic title='Total 24h Volume' value={millify(total24hVolume)}/></Col>
         <Col span={12}><Statistic title='Total Markets' value={millify(totalMarkets)}/></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3}><Link to="/news">Show more</Link></Title>
+      </div>
+      <News simplified />
     </>
+
   )
 }
 
-export default Homepage
+export default Homepage;
